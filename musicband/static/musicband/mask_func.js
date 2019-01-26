d88c5cc047e75c71d844d6ee97474d1eac5d6096
Gagih3 <gagih3@gmail.com>
2019-01-27 02:02:13 +0400

    забожил с маской для телефонов

--- musicband/static/musicband/mask_func.js
+++ musicband/static/musicband/mask_func.js
@@ -1,60 +1,28 @@
-function setCaret(obj,row_index,caret_index) { // перемещение каретки на заданный идекс
-    var range = document.createRange();
-    var selection = window.getSelection();
-    range.setStart(obj.childNodes[row_index],caret_index);
-    range.collapse(true);
-    selection.removeAllRanges();
-    selection.addRange(range)
-}
-
+const new_regex = /(?:^\+|(?<=\+)\d|(?<=\+\d)\-|(?<=\-)\d{1,5}|(?<=\+\d\-\d{5})\-)/gm;
 var targ = $("#11");
-const tel = /^\+7-\d{5}-\d{5}$/; //регулярка телефона российского
-var pattern = "+7-_____-_____";
-var row_couner = 0;
-var caret_couner = 3;
-$(targ).bind("keydown",function (event) {
-    var target = event.target;
-    event.preventDefault(); // предотвращает стандартные действия(не пишет буквы)
-    if (!isNaN(event.key)){ // позволяет вводить только числа
-        pattern = pattern.replace("_",event.key);
-        caret_couner < 13 ? caret_couner++ : caret_couner;
-        if(row_couner === 0){
-            target.innerHTML = "<div>"+ pattern.slice(0,caret_couner >= 8 ? caret_couner + 1: caret_couner) + "</div>";
-            setCaret(target.childNodes[row_couner],row_couner,caret_couner >= 8 ? caret_couner + 1: caret_couner);
+const allowed_keys = [13,40,43,45,48,49,50,51,52,53,54,55,56,57];
+$(targ).keypress(function (e) {
+    if(allowed_keys.includes(e.keyCode)){ // разрешает вводить только цифры и + -
+        var str = e.target.innerText + e.key; // строка для сравнения (падает правильно все)
+        let m_result = "";
+        let i_result = "";
+        let m;
+        while ((m = new_regex.exec(str)) !== null) {
+            if (m.index === new_regex.lastIndex) {
+                new_regex.lastIndex++;
+            }
+            m.forEach((match) => {
+                m_result+=match;
+                i_result=m.input.replace(/Enter|\r?\n|\r/g,"");
+            });
+        }
+        if(m_result==i_result && m_result !== ""){
+            return true;
         }else{
-            target.childNodes[row_couner].innerText = pattern.slice(0,caret_couner >= 8 ? caret_couner + 1: caret_couner);
-            setCaret(target.childNodes[row_couner],0,caret_couner >= 8 ? caret_couner + 1: caret_couner);
+            return false;
         }
-    }else{
-        switch (event.keyCode){
-            case 13: // enter
-                if(tel.test(pattern)){ // pattern refresh if it full
-                    pattern = pattern.replace(/\d{5}-\d{5}/,"_____-_____");
-                    row_couner ++; // row append
-                    caret_couner = 3; //caret move
-                    $(target).append("<div></div>");
-                    console.log(target.childNodes[row_couner])
-                }
-                break;
-            case 8: // backspace
-                if(row_couner == 0){
-                    caret_couner === 3 ? caret_couner : caret_couner--;
-                    pattern = pattern.replace(/\d(?=_)|\d$|\d(?=-_{5}$)/,"_");
-                    target.innerText = pattern.slice(0,caret_couner <= 8 ? caret_couner : caret_couner +1);
-
-                }else{
-                    caret_couner === 3 ? caret_couner : caret_couner--;
-                    pattern = pattern.replace(/\d(?=_)|\d$|\d(?=-_{5}$)/,"_");
-                    target.childNodes[row_couner].innerText = pattern.slice(0,caret_couner <= 8 ? caret_couner : caret_couner +1);
-
-                }
-                break;
-            case 17 && 86: // ctrl + v
-
-                break;
-            default:
-                break;
-        }
-    }
+    }else {
+        return false;
+    };
 });
 
