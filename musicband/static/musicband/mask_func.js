e320b5eb946059d8990b762b1d95a84bae2f63c6
Gagih3 <gagih3@gmail.com>
2019-01-27 11:33:12 +0400

    маска для телефона всё лучше и лучше

--- musicband/static/musicband/mask_func.js
+++ musicband/static/musicband/mask_func.js
@@ -1,7 +1,14 @@
 const new_regex = /(?:^\+|(?<=\+)\d|(?<=\+\d)\-|(?<=\-)\d{1,5}|(?<=\+\d\-\d{5})\-)/gm;
 var targ = $("#11");
-const allowed_keys = [13,40,43,45,48,49,50,51,52,53,54,55,56,57];
-$(targ).keypress(function (e) {
+const allowed_keys = [187,189,48,49,50,51,52,53,54,55,56,57,107,109];
+const meta_keys = [8,13,17,86,90,37,38,39,40,46];
+$(targ).keydown(function (e) {
+    if (meta_keys.includes(e.keyCode)){
+        if (e.ctrlKey && e.keyCode === 86){
+            console.log("ctrl + v")
+        }
+        return true
+    }
     if(allowed_keys.includes(e.keyCode)){ // разрешает вводить только цифры и + -
         var str = e.target.innerText + e.key; // строка для сравнения (падает правильно все)
         let m_result = "";
@@ -16,13 +23,14 @@ $(targ).keypress(function (e) {
                 i_result=m.input.replace(/Enter|\r?\n|\r/g,"");
             });
         }
-        if(m_result==i_result && m_result !== ""){
+        console.log(i_result);
+        if(m_result===i_result && m_result !== ""){
             return true;
         }else{
             return false;
         }
     }else {
         return false;
-    };
+    }
 });
 
