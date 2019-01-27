f63dcb83bf19246054035b9591b0c4cdd704f8c4
Gagih3 <gagih3@gmail.com>
2019-01-27 21:54:11 +0400

    промоушен

--- musicband/static/musicband/mask_func.js
+++ musicband/static/musicband/mask_func.js
@@ -1,17 +1,25 @@
 var listener =  function (event) {
-    let data = event.clipboardData.getData("text");
-    console.log(data.match(tel));
-};
+    if (event.type === "paste") {
+        var data = event.clipboardData.getData("text/plain");
+        if (!data.match(tel)){ // если не соответсвует регексу то не пускает
+            event.preventDefault();
+        }
+    }};
 
 const new_regex = /(?:^\+|(?<=\+)\d|(?<=\+\d)\-|(?<=\-)\d{1,5}|(?<=\+\d\-\d{5})\-)/gm;
 const tel = /\+\d\-\d{5}\-\d{5}/gm;
 var targ = $("#11");
 const allowed_keys = [187,189,48,49,50,51,52,53,54,55,56,57,107,109];
-const meta_keys = [8,13,17,37,38,39,40,46];
+const meta_keys = [8,13,17,37,38,39,40,46,88];
 $(targ).bind("keydown",function (e) {
-    let ctrl_v = e.ctrlKey && e.keyCode === 86,ctrl_z = e.ctrlKey && e.keyCode === 90,ctrl_c = e.ctrlKey && e.keyCode === 67;
-    if (ctrl_c || ctrl_z || ctrl_v || meta_keys.includes(e.keyCode)){ // стандартные команды
-        e.target.addEventListener("paste",listener,false);
+    let ctrl_v = e.ctrlKey && e.keyCode === 86,
+        ctrl_z = e.ctrlKey && e.keyCode === 90,
+        ctrl_c = e.ctrlKey && e.keyCode === 67,
+        ctrl_x = e.ctrlKey && e.keyCode === 88;
+    if (ctrl_x || ctrl_c || ctrl_z || ctrl_v || meta_keys.includes(e.keyCode)){ // стандартные команды
+        ['cut', 'copy', 'paste'].forEach(function(event) {
+            e.target.addEventListener(event,listener,false);
+        });
         return true;
     }
     e.target.removeEventListener("paste",listener,false); // немножко оптимизаций для того что бы не вызывать одно и тоже 100 раз
